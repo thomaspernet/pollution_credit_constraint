0b80d95c8aadee3a484fc16545aa4d3805b28ba9
Thomas <t.pernetcoudrier@gmail.com>
2021-03-24 10:40:34 +0100

    Create file china_spatial_relocation
    169_Pollution_and_Credit_Constraint

---
+++ 01_data_preprocessing/00_download_data/TARGET_SO2/china_cities_target_so2.py
@@ -0,0 +1,163 @@
+import pandas as pd
+from awsPy.aws_s3 import service_s3
+from awsPy.aws_glue import service_glue
+from awsPy.aws_authorization import aws_connector
+from GoogleDrivePy.google_drive import connect_drive
+from GoogleDrivePy.google_authorization import authorization_service
+from pathlib import Path
+import os, re
+import json
+from tqdm import tqdm
+
+### Connect to Cloud providers
+path = os.getcwd()
+parent_path = str(Path(path).parent.parent.parent)
+name_credential = 'financial_dep_SO2_accessKeys.csv'
+region = 'eu-west-3'
+bucket = 'datalake-datascience'
+path_cred = "{0}/creds/{1}".format(parent_path, name_credential)
+
+#### AWS
+con = aws_connector.aws_instantiate(credential=path_cred,
+                                    region=region)
+client = con.client_boto()
+s3 = service_s3.connect_S3(client=client,
+                           bucket=bucket, verbose=True)
+PATH_S3 = "DATA/ENVIRONMENT/CHINA/FYP/SPATIAL_RELOCATION"  ### Copy destination in S3 without bucket and "/" at the end
+### GCP
+auth = authorization_service.get_authorization(
+    #path_credential_gcp=os.path.join(parent_path, "creds", "service.json"),
+    path_credential_drive=os.path.join(parent_path, "creds"),
+    verbose=False,
+    #scope = ['https://www.googleapis.com/auth/spreadsheets.readonly',
+    #"https://www.googleapis.com/auth/drive"]
+)
+gd_auth = auth.authorization_drive(path_secret = os.path.join(parent_path, "creds", "credentials.json"))
+drive = connect_drive.drive_operations(gd_auth)
+
+### DOWNLOAD SPREADSHEET TO temporary_local_data folder
+FILENAME_SPREADSHEET = "China_cities_target_so2"
+spreadsheet_id = drive.find_file_id(FILENAME_SPREADSHEET, to_print=False)
+sheetName = 'relocation'
+var = (
+    drive.upload_data_from_spreadsheet(
+        sheetID=spreadsheet_id,
+        sheetName=sheetName,
+        to_dataframe=True)
+)
+
+### READ DATA
+var.head()
+
+### SAVE LOCALLY
+input_path = os.path.join(parent_path,"00_data_catalog", "temporary_local_data",  FILENAME_SPREADSHEET + ".csv")
+var.to_csv(input_path, index = False)
+
+
+#### SAVE S3
+s3.upload_file(input_path, PATH_S3)
+#os.remove(input_path)
+
+
+for i in var.columns:
+    print({"Name":i, "Type":"float", "Comment":""})
+### ADD SHCEMA
+schema = [
+{'Name': 'citycn', 'Type': 'string', 'Comment': ''},
+{'Name': 'distance', 'Type': 'float', 'Comment': ''},
+{'Name': 'ttoutput_i', 'Type': 'float', 'Comment': ''},
+{'Name': 'ttoutput_j', 'Type': 'float', 'Comment': ''},
+{'Name': 'tso2_mandate_c_j', 'Type': 'float', 'Comment': ''},
+{'Name': 'SO2_perc_reduction_c_j', 'Type': 'float', 'Comment': ''},
+{'Name': 'avg_ij_o_city_mandate', 'Type': 'float', 'Comment': ''},
+{'Name': 'w_avg_ij_o_city_mandate', 'Type': 'float', 'Comment': ''},
+{'Name': 'avg_ij_city_mandate', 'Type': 'float', 'Comment': ''},
+{'Name': 'w_avg_ij_city_mandate', 'Type': 'float', 'Comment': ''},
+{'Name': 'avg_ij_o_perc_city_mandate', 'Type': 'float', 'Comment': ''},
+{'Name': 'w_avg_ij_o_perc_city_mandate', 'Type': 'float', 'Comment': ''},
+{'Name': 'avg_ij_perc_city_mandate', 'Type': 'float', 'Comment': ''},
+{'Name': 'w_avg_ij_perc_city_mandate', 'Type': 'float', 'Comment': ''},
+{'Name': 'd_avg_ij_o_city_mandate_previous', 'Type': 'float', 'Comment': ''},
+{'Name': 'd_w_avg_ij_o_city_mandate', 'Type': 'float', 'Comment': ''},
+{'Name': 'tso2_mandate_c', 'Type': 'float', 'Comment': ''},
+{'Name': 'SO2_perc_reduction_c', 'Type': 'float', 'Comment': ''},
+{'Name': 'output_diff', 'Type': 'float', 'Comment': ''},
+{'Name': 'target_diff', 'Type': 'string', 'Comment': ''},
+{'Name': 'd_avg_ij_o_city_mandate', 'Type': 'string', 'Comment': ''}
+]
+
+
+### ADD DESCRIPTION
+description = 'Use the spreadsheet to download spatia relocation data. In the spreadsheet, construct the dummy as in the notebook to avoid computing it with Athena'
+
+glue = service_glue.connect_glue(client=client)
+
+target_S3URI = os.path.join("s3://datalake-datascience", PATH_S3)
+name_crawler = "crawl-industry-name"
+Role = 'arn:aws:iam::468786073381:role/AWSGlueServiceRole-crawler-datalake'
+DatabaseName = "policy"
+TablePrefix = 'china_' ## add "_" after prefix, ex: hello_
+
+
+glue.create_table_glue(
+    target_S3URI,
+    name_crawler,
+    Role,
+    DatabaseName,
+    TablePrefix,
+    from_athena=False,
+    update_schema=schema,
+)
+
+# Add tp ETL parameter files
+filename = 'china_cities_target_so2.py'
+path_to_etl = os.path.join(str(Path(path).parent.parent.parent), 'utils','parameters_ETL_pollution_credit_constraint.json')
+with open(path_to_etl) as json_file:
+    parameters = json.load(json_file)
+github_url = os.path.join(
+    "https://github.com/",
+    parameters['GLOBAL']['GITHUB']['owner'],
+    parameters['GLOBAL']['GITHUB']['repo_name'],
+    re.sub(parameters['GLOBAL']['GITHUB']['repo_name'],
+           '', re.sub(
+               r".*(?={})".format(parameters['GLOBAL']['GITHUB']['repo_name'])
+               , '', path))[1:],
+    filename
+)
+table_name = '{}{}'.format(TablePrefix, os.path.basename(target_S3URI).lower())
+json_etl = {
+    'description': description,
+    'schema': schema,
+    'partition_keys': [],
+    'metadata': {
+        'DatabaseName': DatabaseName,
+        'TablePrefix': TablePrefix,
+        'TableName': table_name,
+        'target_S3URI': target_S3URI,
+        'from_athena': 'False',
+        'filename': filename,
+        'github_url':github_url
+    }
+}
+
+
+with open(path_to_etl) as json_file:
+    parameters = json.load(json_file)
+
+#parameters['TABLES']['CREATION']['ALL_SCHEMA'].pop(0)
+
+index_to_remove = next(
+                (
+                    index
+                    for (index, d) in enumerate(parameters['TABLES']['CREATION']['ALL_SCHEMA'])
+                    if d['metadata']['filename'] == filename
+                ),
+                None,
+            )
+if index_to_remove != None:
+    parameters['TABLES']['CREATION']['ALL_SCHEMA'].pop(index_to_remove)
+
+parameters['TABLES']['CREATION']['ALL_SCHEMA'].append(json_etl)
+
+with open(path_to_etl, "w")as outfile:
+    json.dump(parameters, outfile)
